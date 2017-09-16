b51955aac06da9ec7a2dd9621e573d13c3b93cfb
agheith <amerghth@gmail.com>
2017-09-16 12:21:51 -0700

    Integrated react-modal library

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
-const Header = (props) => {
-    return (
-      <div>
-        <h1>{props.title}</h1>
-        {props.subtitle &&  <h2>{props.subtitle}</h2>}
-      </div>
-    );
-};
+const Header = (props) => (
 
-Header.defaultProps = {
-    title: "indecision"
-}
+    <div>
+      <h1>{props.title}</h1>
+      {props.subtitle &&  <h2>{props.subtitle}</h2>}
+    </div>
+);
+
 
 export default Header;
