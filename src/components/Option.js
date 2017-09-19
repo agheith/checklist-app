a6be208176dd411a6da912a27e3ed20321a664fb
agheith <amerghth@gmail.com>
2017-09-18 21:56:08 -0700

    Complete Styling the app

--- src/components/Option.js
+++ src/components/Option.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Option = (props) => (
 
-    <div>
-      {props.optionText}
-      <button onClick= {(e) => {
+    <div className="option">
+      <p className="option__text">{props.count}. {props.optionText} </p>
+      <button
+          className="button button--link"
+          onClick= {(e) => {
           props.handleAddOption(props.optionText);
       }}>
       Remove
